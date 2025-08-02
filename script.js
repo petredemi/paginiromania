9d9a7ba3984ec5f2298cdd77cb14c4126a9dd381
petrudemian <petredemi@gmail.com>
2025-08-02 17:49:02 +0100

    worked at dropmenu

--- script.js
+++ script.js
@@ -40,6 +40,7 @@ let run = 0;
 function changeimg(){
     if( run == 13){ run = 0}
     myimage.src = picturemix[run]
+    myimage.setAttribute('style', 'width: 450px; transition: width 1s')
     run += 1
 }
 setInterval(changeimg, 4000)
@@ -62,12 +63,12 @@ dropbtn.addEventListener('click', (e) => {
 navbar.addEventListener('click', (e) => {
     divwidth = toppage.offsetWidth
     if(divwidth < 550){
-        navbar.setAttribute('style', 'display:none')
+        navbar.setAttribute('style', 'widtt:0px; transition: width 1s;')
     }
 })
 dropmenu.addEventListener('mouseleave', (e) => {
     divwidth = toppage.offsetWidth
     if(divwidth < 550){
-    navbar.setAttribute('style', 'display:nonee')
+    navbar.setAttribute('style', 'width: 0px; transition: width 1s;')
     }
 })
