5e8208dd76d1f8d49d1b9327bb6b41db3c37555b
petrudemian <petredemi@gmail.com>
2025-08-12 22:41:01 +0100

    added stocks

--- frontpage.js
+++ frontpage.js
@@ -31,4 +31,93 @@ function changeimg(){
 }
 setInterval(changeimg, 4000)
 
+let simbol1 = document.querySelector('div.simbol1')
+let price1 = document.querySelector('div.price1')
+let procent1 = document.querySelector('div.procent1')
+let img1 = document.querySelector('#img1')
 
+let simbol2 = document.querySelector('div.simbol2')
+let price2 = document.querySelector('div.price2')
+let procent2 = document.querySelector('div.procent2')
+let img2 = document.querySelector('#img2')
+
+let simbol3 = document.querySelector('div.simbol3')
+let price3 = document.querySelector('div.price3')
+let procent3 = document.querySelector('div.procent3')
+let img3 = document.querySelector('#img3')
+
+let simbol4 = document.querySelector('div.simbol4')
+let price4 = document.querySelector('div.price4')
+let procent4 = document.querySelector('div.procent4')
+let img4 = document.querySelector('#img4')
+
+let simbol5 = document.querySelector('div.simbol5')
+let price5 = document.querySelector('div.price5')
+let procent5 = document.querySelector('div.procent5')
+let img5 = document.querySelector('#img5')
+
+let simbol6 = document.querySelector('div.simbol6')
+let price6 = document.querySelector('div.price6')
+let procent6 = document.querySelector('div.procent6')
+let img6 = document.querySelector('#img6')
+
+let simbol7 = document.querySelector('div.simbol7')
+let price7 = document.querySelector('div.price7')
+let procent7 = document.querySelector('div.procent7')
+let img7 = document.querySelector('#img7')
+
+let simbol8 = document.querySelector('div.simbol8')
+let price8 = document.querySelector('div.price8')
+let procent8 = document.querySelector('div.procent8')
+let img8 = document.querySelector('#img8')
+
+let simbol9 = document.querySelector('div.simbol9')
+let price9 = document.querySelector('div.price9')
+let procent9 = document.querySelector('div.procent9')
+let img9 = document.querySelector('#img9')
+
+let igr = './icons/trianglered.png'
+let igg = './icons/trianglegreen.png'
+
+
+const symbols = [simbol1, simbol2, simbol3, simbol4, simbol5, simbol6, simbol7, simbol8, simbol9 ]
+const prices = [price1, price2, price3, price4, price5, price6, price7, price8, price9]
+const procent = [procent1, procent2, procent3, procent4, procent5, procent6, procent7, procent8, procent9]
+const imgs = [img1, img2, img3, img4, img5, img6, img7, img8, img9]
+
+ 
+
+async function finnhubPrice(x, n){
+    try{
+       const response = await fetch(`https://finnhub.io/api/v1/quote?symbol=${x}&exchange=US&token=d2c3hr9r01qvh3vdtos0d2c3hr9r01qvh3vdtosg`, {mode:'cors'});
+       const stock = await response.json();      
+        if(!response.ok){
+            throw 'no data'
+        }else{
+            console.log(stock)
+        //    console.log(stock.c)
+       // console.log(close)
+            let trend = stock.dp
+            symbols[n].textContent = x + ':'
+            prices[n].textContent = stock.c;
+            procent[n].textContent = stock.dp.toFixed(2) + '%'
+            console.log(trend)
+            if(stock.dp > 0){
+            imgs[n].src = igg
+            }else if (stock.dp < 0){
+                imgs[n].src = igr
+            }
+        }
+    }catch(error){
+        console.log(error)
+    };
+}
+finnhubPrice('AMZN', 0)
+finnhubPrice('AAPL', 1)
+finnhubPrice('GOOG', 2)
+finnhubPrice('META', 3)
+finnhubPrice('TSLA', 4)
+finnhubPrice('NVDA', 5)
+finnhubPrice('MSFT', 6)
+finnhubPrice('AVGO', 7)
+finnhubPrice('AMD', 8)
